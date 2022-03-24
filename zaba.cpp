64fc02884df455d3873b2ad8595f00f85e7ae3bc
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-24 21:23:37 +0100

    Update zaba.cpp
    dodanie wsad do klawiszy sterujacych, dodanie metody umozniwiajace wyswietlenie samochodow na ekranie

--- zaba.cpp
+++ zaba.cpp
@@ -1,19 +1,33 @@
 #include "Zaba.h"
+#include "Samochod.h"
+#include "Drugi_samochod.h"
+#include <QGraphicsScene>
 #include <QKeyEvent>
 
-void Zaba::keyPressEvent(QKeyEvent *event){
-    if (event->key() == Qt::Key_Left){
+void Zaba::keyPressEvent(QKeyEvent *event)  {
+    if (event->key() == Qt::Key_Left || event->key() == 'A'){
         if (pos().x() > 0)
-        setPos(x()-10,y());
+           setPos(x()-10,y());
     }
-    else if (event->key() == Qt::Key_Right){
-        if (pos().x() + 50 > 800)
-        setPos(x()+10,y());
+    else if (event->key() == Qt::Key_Right || event->key() == 'D')  {
+        if(pos().x() + 50 < 600)
+            setPos(x()+10,y());
     }
-    else if (event->key() == Qt::Key_Up){
-        setPos(x(),y()-10);
+    else if (event->key() == Qt::Key_Up  || event->key() == 'W')  {
+        if(pos().y() > 0)
+             setPos(x(),y()-10);
     }
-    else if (event->key() == Qt::Key_Down){
+    else if (event->key() == Qt::Key_Down || event->key() == 'S')  {
+        if(pos().y() + 50 < 400 )
         setPos(x(),y()+10);
     }
+}
+
+void Zaba::spawn() {
+        //tworzenie samochodu
+    Samochod *samochod = new Samochod();
+    scene()->addItem(samochod);
+    Drugi_samochod *lewy = new Drugi_samochod();
+    scene()->addItem(lewy);
+
 }
