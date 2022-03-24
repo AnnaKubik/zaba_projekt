76251a8996d789d5778c9e967e690f9d9819460e
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-24 21:21:39 +0100

    Update Zaba.h

--- Zaba.h
+++ Zaba.h
@@ -2,12 +2,15 @@
 #define ZABA_H
 
 #include <QGraphicsRectItem>
-
+#include <QObject>
 //
-class Zaba: public QGraphicsRectItem  {
+class Zaba: public QObject, public QGraphicsRectItem  {
+    Q_OBJECT
 public:
     //metoda ktora zmusza oblekt do ruchy - deklaracja
     void keyPressEvent(QKeyEvent * event);
+public slots:
+    void spawn();
 };
 
 #endif // ZABA_H
