34e63426f2eccb7fac746917516dcbe0df1154b5
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-27 21:57:08 +0200

    Update and rename Drugi_samochod.h to Prawy.h
    zmiana nazwy

---
+++ Prawy.h
@@ -0,0 +1,16 @@
+#ifndef PRAWY_H
+#define PRAWY_H
+#include <QGraphicsRectItem>
+#include <QObject>
+#include "Pojazd.h"
+
+
+class Prawy : public Pojazd  {
+    Q_OBJECT
+public:
+    Prawy();
+public slots:
+    virtual void move();
+};
+
+#endif // PRAWY_H
