ef482cfcface0c5c2ff46769ae702511a5b0386d
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-28 21:43:28 +0200

    Update Zaba.h
    dodanie grafiki

--- Zaba.h
+++ Zaba.h
@@ -1,12 +1,14 @@
 #ifndef ZABA_H
 #define ZABA_H
 
-#include <QGraphicsRectItem>
+#include <QGraphicsPixmapItem>
 #include <QObject>
+#include <QGraphicsScene>
 //
-class Zaba: public QObject, public QGraphicsRectItem  {
+class Zaba: public QObject, public QGraphicsPixmapItem  {
     Q_OBJECT
 public:
+    Zaba(QGraphicsItem * parent=0);
     //metoda ktora zmusza oblekt do ruchy - deklaracja
     void keyPressEvent(QKeyEvent * event);
 public slots:
