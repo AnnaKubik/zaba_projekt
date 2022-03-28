d65622989016ff29cde69591d5715cf4417794da
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-28 21:44:12 +0200

    Update Akcja.cpp
    dodanie tla

--- Akcja.cpp
+++ Akcja.cpp
@@ -4,25 +4,29 @@
 #include <QGraphicsView>
 #include <QTimer>
 #include "Akcja.h"
+#include <QImage>
+
 
 Akcja::Akcja(QWidget *parent)  {
     // tworzenie sceny
      QGraphicsScene * scene = new QGraphicsScene();
      //ust wielkosc sceny
      scene->setSceneRect(0,0,600,400);
+     //dodawanie tla
+     setBackgroundBrush(QBrush(QImage(":/dodatki/tlo.png")));
 
      setScene(scene);
     //tworzenie przedmiotu w scenie - poznieij bedzie to zaba
     Zaba * uzytkownik = new Zaba();
     //ustawianie wielkosci kwadratu - pozniej zaby
     // pierwsze dwa -lolalizacja lewego gornego rogu x,y, kolejne dwa szerokosc i wysokosc
-    uzytkownik->setRect(0,0,50,50);
+    uzytkownik->setPixmap(QPixmap(":/dodatki/zaba.png"));;
     //dodawanie przedmiotu do sceny
     scene->addItem(uzytkownik);
     //dod widoku zbey zobaczyc nasza scene
     QGraphicsView * view = new QGraphicsView(scene);
 
-    uzytkownik->setPos(300,350);
+    uzytkownik->setPos(300,300);
     // skupienie na obiekcie- zaba
     uzytkownik->setFlag(QGraphicsItem::ItemIsFocusable);
     uzytkownik->setFocus();
@@ -42,7 +46,8 @@ Akcja::Akcja(QWidget *parent)  {
 
     QTimer * timer = new QTimer();
     QObject::connect(timer,SIGNAL(timeout()),uzytkownik,SLOT(spawn()));
-    timer->start(5000);
+    timer->start(3000 );
+
 
     show();
 }
