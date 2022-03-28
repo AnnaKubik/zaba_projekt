d9c03d975fdb609424a0f33613a5ba66c1a545be
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-28 21:47:05 +0200

    Update zaba.cpp
    niedzialajaca wersja robocza

--- zaba.cpp
+++ zaba.cpp
@@ -6,26 +6,35 @@
 #include <QKeyEvent>
 #include <QGraphicsView>
 
-Zaba::Zaba (QGraphicsItem *parent): QGraphicsRectItem(parent)  {
+
+
+Zaba::Zaba (QGraphicsItem *parent): QGraphicsPixmapItem(parent) {
 
 }
 void Zaba::keyPressEvent(QKeyEvent *event)  {
     if (event->key() == Qt::Key_Left || event->key() == 'A'){
         if (pos().x() > 0)
-           setPos(x()-10,y());
+           setPos(x()-15,y());
     }
     else if (event->key() == Qt::Key_Right || event->key() == 'D')  {
         if(pos().x() + 50 < 600)
-            setPos(x()+10,y());
+            setPos(x()+15,y());
     }
     else if (event->key() == Qt::Key_Up  || event->key() == 'W')  {
         if(pos().y() > 0)
-             setPos(x(),y()-10);
+             setPos(x(),y()-15);
+//        else if (pos().y() < 0) {
+//            setBackgroundBrush(QBrush(QImage(":/dodatki/wygrana.png")));
+//        }
     }
     else if (event->key() == Qt::Key_Down || event->key() == 'S')  {
         if(pos().y() + 50 < 400 )
-        setPos(x(),y()+10);
+        setPos(x(),y()+15);
     }
+//    if (pos().y() < 0)  {
+//        setBackgroundBrush(QBrush(QImage(":/dodatki/wygrana.png")));
+//        delete this;
+//    }
 
 }
 
