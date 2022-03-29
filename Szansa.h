84d90cb1311232dfafc72eda0ff2f450a2a2cf0e
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-29 14:35:11 +0200

    Create Szansa.h
    dodanie brakujacego pliku, pominiete wczensiej przez nieuwage

---
+++ Szansa.h
@@ -0,0 +1,17 @@
+#ifndef SZANSA_H
+#define SZANSA_H
+#include <QGraphicsTextItem>
+#include "Zaba.h"
+
+
+class Szansa: public QGraphicsTextItem{
+private :
+    int ilosc_szans;
+public:
+    Szansa(QGraphicsItem *parent = 0);
+    void zmniejsz();
+    int podaj_zycie();
+    void gameOver();
+};
+
+#endif // SZANSA_H
