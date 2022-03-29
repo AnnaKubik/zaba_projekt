58cae6ae13de2805babb265856b1b11fcc879b14
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-29 14:35:51 +0200

    Create Szansa.cpp
    dodanie pliku pominietego przez nieuwage

---
+++ Szansa.cpp
@@ -0,0 +1,49 @@
+#include "Szansa.h"
+#include <QFont>
+#include "Zaba.h"
+
+Szansa::Szansa(QGraphicsItem *parent): QGraphicsTextItem(parent)  {
+    // ustaw poczatek zycia jako 2
+    ilosc_szans = 2;
+
+    //wyswietlanie zycia
+    setPlainText(QString("Ilosc szans: ") + QString::number(ilosc_szans));
+    // ust koloru
+    setDefaultTextColor(Qt::green);
+    // ust czcionki
+    setFont(QFont("",20));
+}
+void Szansa::gameOver()  {
+    //policz ile zyci i daj wynik
+    int pozostale_szanse = 0;
+    int obecne_szanse = 0;
+    if (ilosc_szans > 0) {
+
+    } else if (ilosc_szans == 0)  {
+        setPlainText(QString("Koniec gry") );
+       setDefaultTextColor(Qt::red);
+       setFont(QFont("",80));
+    }
+    else if(ilosc_szans < 0) {
+        exit(0);
+    }
+
+}
+void Szansa::zmniejsz()  {
+    ilosc_szans--;
+    //wyswietlanie zycia po zmianie (po uderzeniu w samochod)
+     setPlainText(QString("Ilosc szans: ") + QString::number(ilosc_szans));
+//     if (ilosc_szans ==1) {
+//       setPlainText(QString("Ostatnia szansa") );
+//      setDefaultTextColor(Qt::red);
+//      setFont(QFont("",40));
+//     }
+     if (ilosc_szans <= 0) {
+      gameOver();
+     }
+}
+int Szansa::podaj_zycie()  {
+    return ilosc_szans;
+}
+
+
