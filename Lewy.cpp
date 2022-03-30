12922069760d6b44b8502b9fb2910a25878e61a6
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-30 23:01:25 +0200

    Update Lewy.cpp
    znaczace zmiany z dziedziczeniu funkcji

--- Lewy.cpp
+++ Lewy.cpp
@@ -6,7 +6,7 @@
 #include "Szansa.h"
 #include "Zaba.h"
 #include "Akcja.h"
-extern Akcja * akcja;
+//extern Akcja * akcja;
 Lewy::Lewy()  {
     //miejsce startu
     setPos(0,140);
@@ -18,36 +18,35 @@ Lewy::Lewy()  {
 
 //    t_l->start(50);
 }
-//w prawo
-void Lewy::move()  {
-     QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
+//w lewo
+void Lewy::ruch()  {
+//     QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
 
-    // jezeli samochod zderzy sie z zaba zniszcz oba
-    for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
-        if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
-            //zmniejsz zycie
-            akcja->szansa->zmniejsz();
+//     // jezeli samochod zderzy sie z zaba zniszcz oba
+//     for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
+//         if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
+//             //zmniejsz zycie
+//             akcja->szansa->zmniejsz();
+//             // usun ze scey(pozostaje na stercie
+//             scene()->removeItem(Zaba_po_zderzeniu[i]);
 
-            // usun ze scey(pozostaje na stercie
-            scene()->removeItem(Zaba_po_zderzeniu[i]);
+//             Zaba_po_zderzeniu[i]->setPos(300,300);
+//             //ponowne dodanie zaby na start
+//             scene()->addItem(Zaba_po_zderzeniu[i]);
 
-        Zaba_po_zderzeniu[i]->setPos(300,300);
+//             Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
+//             Zaba_po_zderzeniu[i]->setFocus();
 
-            scene()->addItem(Zaba_po_zderzeniu[i]);
-
-            Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
-            Zaba_po_zderzeniu[i]->setFocus();
-
-
-            return;
-        }
-    }
-
-    int random_number = rand() % 6;
-  setPos(x()+random_number,y());
-  if (pos().x() + pixmap().width() > 650)  {
-      scene()->removeItem(this);
-      delete this;
-  }
-
+//             return;
+//        }
+//    }
+    // ruch w lewo
+     int random_number = rand() % 8;
+     setPos(x()+random_number,y());
+     if (pos().x() + pixmap().width() > 650)  {
+         scene()->removeItem(this);
+         delete this;
+     }
+     Lewy::po_uderzeniu();
 }
+
