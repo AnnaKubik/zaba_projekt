3c97c973b592a189a2fd4b40e1d6b96bd4d7956a
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-30 23:02:54 +0200

    Update Prawy.cpp
    znaczace zmniany w dziedziczeniu metody

--- Prawy.cpp
+++ Prawy.cpp
@@ -8,46 +8,45 @@
 #include "Szansa.h"
 #include <QImage>
 //objekt globalny
-extern Akcja * akcja;
+//extern Akcja * akcja;
 Prawy::Prawy()  {
-    // set random position
+    // meijsce startu
     setPos(500,260);
     //rysowanie samochodu
-     setPixmap(QPixmap(":/dodatki/samochod_prawy.png"));
+    setPixmap(QPixmap(":/dodatki/samochod_prawy.png"));
 }
 //w prawo
-void Prawy::move()  {
+void Prawy::ruch()  {
+//    QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
 
-    QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
+//    // jezeli samochod zderzy sie z zaba zniszcz oba
+//    for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
+//        if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
+//            //zmniejsz zycie
+//            akcja->szansa->zmniejsz();
+//            // usun ze scey(pozostaje na stercie
+//            scene()->removeItem(Zaba_po_zderzeniu[i]);
 
-    // jezeli samochod zderzy sie z zaba zniszcz oba
-    for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
-        if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
-            //zmniejsz zycie
-            akcja->szansa->zmniejsz();
+//            Zaba_po_zderzeniu[i]->setPos(300,300);
 
-            // usun ze scey(pozostaje na stercie
-            scene()->removeItem(Zaba_po_zderzeniu[i]);
+//            scene()->addItem(Zaba_po_zderzeniu[i]);
 
-        Zaba_po_zderzeniu[i]->setPos(300,300);
+//            Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
+//            Zaba_po_zderzeniu[i]->setFocus();
 
-            scene()->addItem(Zaba_po_zderzeniu[i]);
+//            return;
+//        }
+//    }
 
-            Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
-            Zaba_po_zderzeniu[i]->setFocus();
-
-
-            return;
-        }
-    }
-
-
-
-    int random_number = rand() % 5;
-  setPos(x()-random_number,y());
-  if (pos().x() + pixmap().width() < 0)  {
-      scene()->removeItem(this);
-      delete this;
+//ruch w prawo
+    int random_number = rand() % 6;
+    setPos(x()-random_number,y());
+    if (pos().x() + pixmap().width() < 0)  {
+        scene()->removeItem(this);
+        delete this;
   }
+    Prawy::po_uderzeniu();
 }
+
+
 
