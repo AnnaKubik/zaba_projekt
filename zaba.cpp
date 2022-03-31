a264d6defee00c1943efa5a9dcccc7b9a2ddb427
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-03-31 21:33:24 +0200

    Update zaba.cpp
    definicja funkcji wygrana, zaba po dodatci na druga strone ulice nie moze sie cofnac oraz zmienia się  jej wyglad

--- zaba.cpp
+++ zaba.cpp
@@ -1,48 +1,49 @@
 #include "Zaba.h"
-#include "Pojazd.h"
+#include "Akcja.h"
 #include "Lewy.h"
 #include "Prawy.h"
 #include <QGraphicsScene>
 #include <QKeyEvent>
 #include <QGraphicsView>
 
-
-
-Zaba::Zaba (QGraphicsItem *parent): QGraphicsPixmapItem(parent) {
-
+Zaba::Zaba(QGraphicsItem *parent): QGraphicsPixmapItem(parent)  {
+    //pozycja startowa zaby
+    setPos(260,300);
+    // skupienie na obiekcie- zaba
+    setFlag(QGraphicsItem::ItemIsFocusable);
+    setFocus();
+    //rysowanie zaby
+    setPixmap(QPixmap(":/dodatki/zaba.png"));
 }
+
 void Zaba::keyPressEvent(QKeyEvent *event)  {
-    if (event->key() == Qt::Key_Left || event->key() == 'A'){
+
+    if (event->key() == Qt::Key_Left || event->key() == 'A')  {
         if (pos().x() > 0)
            setPos(x()-15,y());
     }
     else if (event->key() == Qt::Key_Right || event->key() == 'D')  {
-        if(pos().x() + 50 < 600)
+        if(pos().x() + 83 < 600)
             setPos(x()+15,y());
     }
     else if (event->key() == Qt::Key_Up  || event->key() == 'W')  {
-        if(pos().y() > 0)
-             setPos(x(),y()-15);
-//        else if (pos().y() < 0) {
-//            setBackgroundBrush(QBrush(QImage(":/dodatki/wygrana.png")));
-//        }
+        if(pos().y() > 0) {
+             setPos(x(),y() - 15);
+             wygrana();
+        }
     }
     else if (event->key() == Qt::Key_Down || event->key() == 'S')  {
-        if(pos().y() + 50 < 400 )
+        if(pos().y() > 100 && pos().y() + 99 < 400 )
         setPos(x(),y()+15);
     }
-//    if (pos().y() < 0)  {
-//        setBackgroundBrush(QBrush(QImage(":/dodatki/wygrana.png")));
-//        delete this;
-//    }
 
 }
 
-void Zaba::spawn() {
-        //tworzenie samochodu
-    Prawy *prawy = new Prawy();
-    scene()->addItem(prawy);
-    Lewy *lewy = new Lewy();
-    scene()->addItem(lewy);
-
+void Zaba::wygrana() {
+    if(pos().y() < 5)
+        setPixmap(QPixmap(":/dodatki/wygrana_zaba.png"));
 }
+
+
+
+
