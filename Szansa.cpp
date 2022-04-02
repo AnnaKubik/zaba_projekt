508ec1a78f17aed57bbabdc533a98292ea0d97dd
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-04-02 19:45:50 +0200

    Update Szansa.cpp
    utowrzenie nowej metody zwieksz - zwiekszajacej zycie

--- Szansa.cpp
+++ Szansa.cpp
@@ -1,6 +1,6 @@
 #include "Szansa.h"
 #include <QFont>
-#include "Zaba.h"
+
 
 Szansa::Szansa(QGraphicsItem *parent): QGraphicsTextItem(parent)  {
     // ustaw poczatek zycia jako 2
@@ -15,35 +15,27 @@ Szansa::Szansa(QGraphicsItem *parent): QGraphicsTextItem(parent)  {
 }
 void Szansa::gameOver()  {
     //policz ile zyci i daj wynik
-    /*if (ilosc_szans > 0) {
-
-    } else*/ if (ilosc_szans == 0)  {
+    if (ilosc_szans == 0)  {
         setPlainText(QString("Koniec gry") );
-       setDefaultTextColor(Qt::red);
-       setFont(QFont("",80));
+        setDefaultTextColor(Qt::red);
+        setFont(QFont("",80));
     }
     else if(ilosc_szans < 0) {
-        exit(0);
+        //exit(0);
     }
-
 }
 
 // funkcja odbierajaca ilosc szans po kazdym uderzeniu w samochod
 void Szansa::zmniejsz()  {
     ilosc_szans--;
     //wyswietlanie zycia po zmianie (po uderzeniu w samochod)
-     setPlainText(QString("Ilosc szans: ") + QString::number(ilosc_szans));
-//     if (ilosc_szans ==1) {
-//       setPlainText(QString("Ostatnia szansa") );
-//      setDefaultTextColor(Qt::red);
-//      setFont(QFont("",40));
-//     }
-     if (ilosc_szans <= 0) {
-      gameOver();
+    setPlainText(QString("Ilosc szans: ") + QString::number(ilosc_szans));
+    if (ilosc_szans <= 0) {
+        gameOver();
      }
 }
-//int Szansa::podaj_zycie()  {
-//    return ilosc_szans;
-//}
 
-
+void Szansa::zwieksz()  {
+    ilosc_szans++;
+    setPlainText(QString("Ilosc szans: ") + QString::number(ilosc_szans));
+}
