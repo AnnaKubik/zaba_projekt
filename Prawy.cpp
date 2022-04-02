10d350a7c82495c556b9b45f0ce9c818da92fcdb
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-04-02 19:44:40 +0200

    Update Prawy.cpp
    ostateczna wersja

--- Prawy.cpp
+++ Prawy.cpp
@@ -3,12 +3,7 @@
 #include <QGraphicsScene>
 #include <QList>
 #include <stdlib.h>
-#include "Zaba.h"
-#include "Akcja.h"
-#include "Szansa.h"
-#include <QImage>
-//objekt globalny
-//extern Akcja * akcja;
+
 Prawy::Prawy()  {
     // meijsce startu
     setPos(500,260);
@@ -17,34 +12,13 @@ Prawy::Prawy()  {
 }
 //w prawo
 void Prawy::ruch()  {
-//    QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
-
-//    // jezeli samochod zderzy sie z zaba zniszcz oba
-//    for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
-//        if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
-//            //zmniejsz zycie
-//            akcja->szansa->zmniejsz();
-//            // usun ze scey(pozostaje na stercie
-//            scene()->removeItem(Zaba_po_zderzeniu[i]);
-
-//            Zaba_po_zderzeniu[i]->setPos(300,300);
-
-//            scene()->addItem(Zaba_po_zderzeniu[i]);
-
-//            Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
-//            Zaba_po_zderzeniu[i]->setFocus();
-
-//            return;
-//        }
-//    }
-
 //ruch w prawo
     int random_number = rand() % 6;
     setPos(x()-random_number,y());
     if (pos().x() + pixmap().width() < 0)  {
         scene()->removeItem(this);
         delete this;
-  }
+    }
     Prawy::po_uderzeniu();
 }
 
