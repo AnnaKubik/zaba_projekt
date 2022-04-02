8497b4f4bdf4e40decdc8276c589862620d73e2c
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-04-02 19:43:14 +0200

    Update Pojazd.cpp
    ostateczna wejsja

--- Pojazd.cpp
+++ Pojazd.cpp
@@ -1,7 +1,6 @@
 #include "Pojazd.h"
 #include <QTimer>
 #include <QGraphicsScene>
-#include <QList>
 #include <stdlib.h>
 #include "Zaba.h"
 #include "Akcja.h"
@@ -16,7 +15,7 @@ Pojazd::Pojazd() : QObject(), QGraphicsPixmapItem()  {
 void Pojazd::po_uderzeniu() {
     QList<QGraphicsItem *> Zaba_po_zderzeniu = collidingItems();
 
-    // jezeli samochod zderzy sie z zaba zniszcz oba
+    // jezeli samochod zderzy sie z zaba zabe
     for (int i = 0, n = Zaba_po_zderzeniu.size(); i < n; ++i)  {
         if (typeid(*(Zaba_po_zderzeniu[i])) == typeid(Zaba))  {
             //zmniejsz zycie
@@ -24,24 +23,18 @@ void Pojazd::po_uderzeniu() {
             // usun ze sceny(pozostaje na stercie
             scene()->removeItem(Zaba_po_zderzeniu[i]);
 
-
             if (akcja->szansa->ilosc_szans <= 0) {
                 scene()->removeItem(Zaba_po_zderzeniu[i]);
-             //   scene()->removeItem(this);
-
 
                  delete Zaba_po_zderzeniu[i];
-                //delete this;
             }  else {
                 //ust nowej pozycji zaby
                 Zaba_po_zderzeniu[i]->setPos(300,300);
                 //ponowne dodanie zaby na start
                 scene()->addItem(Zaba_po_zderzeniu[i]);
 
-
-
-            Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
-            Zaba_po_zderzeniu[i]->setFocus();
+                Zaba_po_zderzeniu[i]->setFlag(QGraphicsItem::ItemIsFocusable);
+                Zaba_po_zderzeniu[i]->setFocus();
 }
             return;
 
