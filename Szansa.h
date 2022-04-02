69f2097e0c574e0a68af7a647b899680b3790515
AnnaKubik <100802652+AnnaKubik@users.noreply.github.com>
2022-04-02 20:04:07 +0200

    Update Szansa.h

--- Szansa.h
+++ Szansa.h
@@ -3,6 +3,8 @@
 #include <QGraphicsTextItem>
 #include "Zaba.h"
 
+//klasa zawiarajaca metody typu zmniejsz, zwieksza i gameOver ktore sa odpowiedzialne za zarzadzanie iloscia pozostalych szans oraz jej nastepstwami
+
 class Szansa: public QGraphicsTextItem  {
 public :
     int ilosc_szans;
